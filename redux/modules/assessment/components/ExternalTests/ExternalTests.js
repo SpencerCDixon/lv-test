70dabd7376696ee8ace3e65529b2a18a7e772ec5
Spencer Dixon <spencercdixon@gmail.com>
2017-01-21 15:15:32 -0500

    Fixes margin in external tests page

--- redux/modules/assessment/components/ExternalTests/ExternalTests.js
+++ redux/modules/assessment/components/ExternalTests/ExternalTests.js
@@ -11,24 +11,24 @@ class ExternalTests extends Component {
       <Flex flexColumn style={{width: '100%'}}>
         <H1 center my={3}>Entering External Test Results</H1>
 
-        <Flex flexColumn mt={4}>
+        <Flex flexColumn mt={2}>
           <EnterTest 
             title="CVI"
             subtitle="Core Values Index. Conflict Resolution"
             to="http://ciyourcvi.com"
-            my={3}
+            my={2}
           />
           <EnterTest 
             title="Time Paradox"
             subtitle="Your orientation to past, present, and future."
             to="http://www.thetimeparadox.com/zimbardo-time-perspective-inventory"
-            my={3}
+            my={2}
           />
           <EnterTest 
             title="16 Personalities"
             subtitle="Your strengths & weaknesses"
             to="https://www.16personalities.com/free-personality-test"
-            my={3}
+            my={2}
           />
         </Flex>
       </Flex>
